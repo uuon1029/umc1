4d8d76097c9a0608bfaf0a945e62da57adcb8eb8
uuon <uuon1029@mail.ulsan.ac.kr>
2024-07-08 16:49:39 +0900

    menu

---
+++ app/src/main/java/com/example/umc1/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.umc1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.umc1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(){
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
