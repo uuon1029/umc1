c104d5f0e303f7743c2f5c00ba62b96d07d59a7b
zerojung <claraa03063@gmail.com>
2024-07-08 15:55:40 +0900

    home

--- app/src/main/java/com/example/umc1/ShopFragment.kt
+++ app/src/main/java/com/example/umc1/ShopFragment.kt
@@ -1,7 +1,23 @@
 package com.example.umc1
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.umc1.databinding.FragmentShopBinding
 
 class ShopFragment : Fragment(){
+    lateinit var binding: FragmentShopBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentShopBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 
 }
