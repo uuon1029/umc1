9a37e33d2677d25e1962c41fed90ca799f7c49b2
zerojung <claraa03063@gmail.com>
2024-07-10 10:58:36 +0900

    sol

---
+++ app/src/main/java/com/example/umc1/TabTest2Fragment.kt
@@ -0,0 +1,25 @@
+package com.example.umc1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.umc1.databinding.FragmentConfigBinding
+import com.example.umc1.databinding.FragmentHomeBinding
+
+class TabTest2Fragment : Fragment(){
+
+    lateinit var binding: FragmentConfigBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentConfigBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
