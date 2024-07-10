49b93174a2ae0f6fd8fea614c4980eeb302d6b33
uuon <uuon1029@mail.ulsan.ac.kr>
2024-07-10 11:17:21 +0900

    viewPager

--- app/src/main/java/com/example/umc1/ConfigFragment.kt
+++ app/src/main/java/com/example/umc1/ConfigFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.umc1.databinding.FragmentConfigBinding
 import com.example.umc1.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class ConfigFragment : Fragment(){
 
     lateinit var binding: FragmentConfigBinding
+
+    private val contents = arrayListOf("test1", "test2")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +21,14 @@ class ConfigFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentConfigBinding.inflate(inflater, container, false)
+
+        val vpAdapter = ConfigVPAdapter(this)
+        binding.configVp.adapter = vpAdapter
+
+        TabLayoutMediator(binding.configTab,binding.configVp){
+            tab,position ->
+            tab.text = contents[position]
+        }.attach()
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/umc1/ConfigVPAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.umc1
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ConfigVPAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment{
+        return when(position) {
+            0 -> TabTest1Fragment()
+            else -> TabTest2Fragment()
+        }
+    }
+
+}
